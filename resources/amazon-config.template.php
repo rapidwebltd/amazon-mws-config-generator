ef3f57c4477b28e5731327e1743bea5df581da0c
Jordan Hall <jordan.hall@rapidweb.biz>
2017-04-07 12:42:25 +0100

    Store name in template

--- resources/amazon-config.template.php
+++ resources/amazon-config.template.php
@@ -1,12 +1,12 @@
 <?php
 
 // Store configuration
-$store['AmazonStore']['merchantId'] = '[[merchantId]]';         // Merchant ID for this store
-$store['AmazonStore']['marketplaceId'] = '[[marketplaceId]]';   // Marketplace ID for this store
-$store['AmazonStore']['keyId'] = '[[keyId]]';                   // Access Key ID
-$store['AmazonStore']['secretKey'] = '[[secretKey]]';           // Secret Access Key for this store
-$store['AmazonStore']['serviceUrl'] = '[[serviceUrl]]';         // Optional override for Service URL
-$store['AmazonStore']['MWSAuthToken'] = '[[MWSAuthToken]]';     // Token needed for web apps and third-party developers
+$store['[[storeName]]']['merchantId'] = '[[merchantId]]';         // Merchant ID for this store
+$store['[[storeName]]']['marketplaceId'] = '[[marketplaceId]]';   // Marketplace ID for this store
+$store['[[storeName]]']['keyId'] = '[[keyId]]';                   // Access Key ID
+$store['[[storeName]]']['secretKey'] = '[[secretKey]]';           // Secret Access Key for this store
+$store['[[storeName]]']['serviceUrl'] = '[[serviceUrl]]';         // Optional override for Service URL
+$store['[[storeName]]']['MWSAuthToken'] = '[[MWSAuthToken]]';     // Token needed for web apps and third-party developers
 
 // Service URL Base (default: 'https://mws.amazonservices.com/')
 $AMAZON_SERVICE_URL = '[[AMAZON_SERVICE_URL]]';
